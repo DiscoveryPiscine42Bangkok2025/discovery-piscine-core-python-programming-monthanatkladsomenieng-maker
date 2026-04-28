f19a0fd8f760cd6de44f17bdc3014d610ec549f6
monthanatkladsomenieng-maker <monthanatkladsomenieng@gmail.com>
2026-04-28 10:36:24 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+First_name = "Wil"
+Last_name = "42"
+print(First_name , Last_name)
+
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+full_name = "Monthanat"
+last_name = "Kladosomenieng"
+whole_name = full_name + " " + last_name
+print(whole_name)
